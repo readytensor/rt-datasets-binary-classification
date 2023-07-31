860222478e94735ca3a207d4d5c248a62225a3f8
Abu Desai <abhyuday.desai@gmail.com>
2023-07-30 17:49:46 -0700

    refactor scripts

--- run_all.py
+++ run_all.py
@@ -1,10 +1,10 @@
 
 from generate_schemas import run_schema_gen
-from create_train_test_key_files import create_train_test_testkey_files
-from generate_inference_data import generate_inference_request_data
+from create_train_test_key_files import run_train_test_testkey_files_gen
+from generate_inference_data import run_inference_request_data_gen
 
 
 if __name__ == "__main__":
     run_schema_gen()
-    create_train_test_testkey_files()
-    generate_inference_request_data()
+    run_train_test_testkey_files_gen()
+    run_inference_request_data_gen()
